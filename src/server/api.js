9402f9d44d3cab5f447d9ff2f23d7ea1e84e35df
Daniel Kostro <kostro.d@gmail.com>
2016-12-19 10:59:17 +0100

    Remove logs

--- src/server/api.js
+++ src/server/api.js
@@ -16,24 +16,19 @@ router.post('/send/:deviceId', function *() {
         if (typeof this.request.body === 'object') {
             const type = Object.prototype.toString.call(this.request.body).slice(8, -1).toLowerCase();
             if (type === 'object' && this.request.body.b64data) {
-                console.log('b64data');
                 data = Buffer.from(this.request.body.b64data, 'base64');
             } else if (JSON.stringify(this.request.body) === '{}') {
-                console.log('raw body');
                 data = yield raw(inflate(this.req), {
                     limit: '1mb'
                 });
-                console.log(data.toString());
             } else {
                 this.status = 400;
                 handleError.call(this, new Error('Bad arguments'));
                 return;
             }
         } else if (typeof this.request.body === 'string') {
-            console.log('string');
             data = this.request.body;
         } else {
-            console.log('bad');
             this.status = 400;
             handleError.call(this, new Error('Bad arguments'));
             return;
