3536980cc186113d211e942dc1453e163d63c88d
Daniel Kostro <kostro.d@gmail.com>
2017-01-18 15:16:55 +0100

    clear command at init

--- src/serial/deviceManager.js
+++ src/serial/deviceManager.js
@@ -2,12 +2,13 @@
 
 const DeviceManager = require('serial-requests').DeviceManager;
 
+const data = [23,23,23, 23, 23, 67, 76, 69, 65, 82, 23, 23, 23, 23, 23].map(String.fromCharCode).join('');
 const deviceManager = new DeviceManager({
     optionCreator: function(portInfo) {
         if (portInfo.manufacturer === 'Keyspan') {
             return {
                 baudRate: 38400,
-                getIdCommand: '!SHOW HOST_NAME\n',
+                getIdCommand: data + '\n' + '!SHOW HOST_NAME\n',
                 getIdResponseParser: function (buffer) {
                     var m = /^Host Name = (.*)\r\n$/.exec(buffer);
                     if (m && m[1]) {
